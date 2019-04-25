a78919c7046538189dff3d167714cd81310e895c
magic-onion <alexander.m.truong@gmail.com>
2019-04-24 22:08:02 -0400

    linking all the lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -34,17 +34,135 @@ class LinkedList {
 
   getLast() {
     let node = this.head
-    let lastNode = {}
+
+    if (!this.head) {
+      return null
+    }
     while (node) {
       if (!node.next) {
-        lastNode = node
+        return node
       }
       node = node.next
     }
     return lastNode
   }
 
-  
+  clear() {
+    this.head = null
+  }
+
+  removeFirst() {
+    if (!this.head.next) {
+      this.clear()
+      return
+    }
+    this.head = this.head.next
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return null
+    }
+    if (!this.head.next) {
+      this.head = null
+      return
+    }
+
+    let node = this.head
+    while (node.next.next) {
+      node = node.next
+    }
+    node.next = null
+  }
+
+  insertLast(data) {
+    let lastNode = this.getLast()
+    if (lastNode) {
+      lastNode.next = new Node(data)
+    }
+    else {
+      this.head = new Node(data)
+    }
+  }
+
+  getAt(integer) {
+    if (!this.head) {
+      return null
+    }
+    if (integer === 0) {
+      return this.head
+    }
+    let count = 0
+    let node = this.head
+    while (node && count < integer) {
+      node = node.next
+      count++
+    }
+    return node
+  }
+
+  removeAt(integer) {
+    if (!this.head) {
+      return null
+    }
+
+    if (integer > this.size() - 1) {
+      return
+    }
+
+    if (integer === 0) {
+      this.head = this.head.next
+      return
+    }
+
+    if (integer === this.size() - 1) {
+      this.removeLast()
+      return
+    }
+
+    let prevNode = this.getAt(integer - 1)
+    let node = prevNode.next
+    if (node.next) {
+      prevNode.next = node.next
+    }
+    else {
+      prevNode.next = null
+    }
+  }
+
+  insertAt(data, integer) {
+    if (!this.head) {
+      this.head = new Node(data)
+      return
+    }
+    if (!this.getAt(integer) || integer > this.size()) {
+      this.insertLast(data)
+      return
+    }
+    if (integer === 0) {
+      this.insertFirst(data)
+      return
+    }
+
+    let prevNode = this.getAt(integer - 1)
+    let node = new Node(data, prevNode.next)
+    prevNode.next = node
+  }
+
+  forEach(fn) {
+    if (!this.head) {
+      return
+    }
+
+    let node = this.head
+    while (node) {
+      fn(node)
+      node = node.next
+    }
+    return
+  }
+
+
 
 }
 
