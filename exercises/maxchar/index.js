c887b54b03d6413a738aa3362a6406ba487144b3
magic-onion <alexander.m.truong@gmail.com>
2019-05-05 13:29:42 -0400

    hashmap

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let hashMap = {}
+  let max = 0
+  let char = ""
+  for (let i = 0; i < str.split("").length; i++) {
+    if (hashMap[str.split("")[i]]) {
+      hashMap[str.split("")[i]] += 1
+    }
+    else {
+      hashMap[str.split("")[i]] = 1
+    }
+  }
+  for (let j in hashMap) {
+    if (hashMap[j] > max) {
+      max = hashMap[j]
+      char = j
+    }
+  }
+  return char
+
+}
 
 module.exports = maxChar;
